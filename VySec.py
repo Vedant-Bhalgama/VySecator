b45e2035b778fec8e9507693e8de317a493284cf
Vedant Bhalgama <67494275+Vedant-Bhalgama@users.noreply.github.com>
2020-07-20 19:14:21 +0530

    Added a New Option

---
+++ VySec.py
@@ -0,0 +1,67 @@
+import subprocess
+import re
+import time
+import pyfiglet
+from pyarmor.pyarmor import main as call_pyarmor
+from os import system, name
+
+def clear_output():
+	# for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+
+ascii_banner = pyfiglet.figlet_format("VySecator")
+print(ascii_banner)
+
+print("VySecator - Program Built for Hackers, By Hackers ;)")
+print("Version : 1.1.3")
+print("\nCHANGELOGS --> ADDED A NEW OPTION, FIXED BUGS")
+time.sleep(5)
+clear_output()
+
+
+def ask_user_choice():
+	print("\n")
+	print("--------------------------------------------")
+	print("\t(1) Obfuscate Python Script")
+	print("\t(2) Compile .py to EXE")
+	print("\t(3) Obfuscate and Compile .py to EXE")
+	print("\t(4) Obfuscate and Compile to .py to EXE with icon file")
+	print("\nFor EG. >> use 1")
+	print("--------------------------------------------")
+	user_input = raw_input("{+} Enter your choice >> ")
+
+	#Main Functions start from here
+
+	if "use 1" in user_input:
+		clear_output()
+		ask_path = raw_input("[+] Please enter script path --> ")
+		subprocess.call("pyarmor obfuscate " + ask_path)
+		print("[+] Scripts were Obfuscated Successfully, Please check the dist folder")
+
+
+	if "use 2" in user_input:
+		clear_output()
+		ask_path = raw_input("[+] Please enter script path --> ")
+		subprocess.call("pyinstaller --onefile " + ask_path)
+		print("[+] Scripts were Compiled Successfully, Please check the dist folder")
+
+
+	if "use 3" in user_input:
+		clear_output()
+		ask_path = raw_input("[+] Please enter script path --> ")
+		call_pyarmor(['pack', '-e', '--onefile --noconsole', ask_path])
+		print("[+] Scripts were Obfuscated and Compiled Successfully, Please check the dist folder")
+		
+	if "use 4" in user_input:
+                clear_output()
+                ask_path = raw_input("[+] Please enter script path --> ")
+                cmd = ['pack', '-e', ' --icon %s --onefile' % "def.ico", ask_path]
+                call_pyarmor(cmd)
+
+
+ask_user_choice()
